89bddba462834b972d536a6e58a8c58e656f9810
braioghie01 <boghees@gmail.com>
2022-12-14 10:40:25 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/* comment*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+
+	} else if (n == 0)
+	{
+		_putchar(48);
+
+		return (0);
+
+	} else if (n < 0)
+	{
+		_putchr('-');
+
+		return (-1);
+	}
+}
