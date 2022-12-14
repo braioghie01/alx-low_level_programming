cf6f066c148457a8f4d72acee853b0a00ad4c328
braioghie01 <boghees@gmail.com>
2022-12-14 13:52:02 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
+ * print_sign -> prints  sign based on condition
  * @n: the int to check
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
