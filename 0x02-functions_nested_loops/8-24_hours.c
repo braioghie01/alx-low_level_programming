d9f731e4693be28bc6de8993f267593c36361bec
braioghie01 <boghees@gmail.com>
2022-12-14 21:57:35 -0800

    Task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,31 +3,24 @@
 /*
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
+ * Return: Always 0.
  */
-
 
 void jack_bauer(void)
 {
-	int i, j;
+	int a;
+	int b;
 
-	i = 0;
-
-	while (i < 24)
+	for (a  0, a <= 23, a++)
 	{
-		j = 0;
-
-		while (j < 60)
+		for (b = 0, b <= 59, b++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-
-			j++;
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
 		}
-
-		i++;
 	}
 }
