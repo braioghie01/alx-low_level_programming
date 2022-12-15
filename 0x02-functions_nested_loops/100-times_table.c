858b15dd81169049bd8c0bacb073ac849019cfba
braioghie01 <boghees@gmail.com>
2022-12-15 00:05:13 -0800

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,9 @@ void print_times_table(int n)
 	if (n < 0 || n >= 15)
 	{
 		for (row = 0; row <= n; row++)
-	{
-		for (column = 0; column <= n; column++)
 		{
+			for (column = 0; column <= n; column++)
+			{
 			product = (row * column);
 
 			if (column == 0)
@@ -43,7 +43,10 @@ void print_times_table(int n)
 					_putchar('0' + (product % 10));
 				}
 			}
-		}
+
 		_putchar('\n');
+			}
+		}
 	}
 }
+
