8425b5ae9e7fa052f319aeed056f43305603cc42
braioghie01 <boghees@gmail.com>
2022-12-14 23:52:43 -0800

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,7 +23,7 @@ void print_times_table(int n)
 			product = (row * column);
 
 			if (column == 0)
-				_putchar('0' + produt);
+				_putchar('0' + product);
 			else
 			{
 				_putchar(',');
@@ -32,7 +32,7 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar('0' + prodct);
+					_putchar('0' + product);
 				}
 				else if (product > 9 && product < 100)
 				{
